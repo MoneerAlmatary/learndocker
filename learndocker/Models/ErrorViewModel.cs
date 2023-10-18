57e1b182d62cde207c926ff931f9ec4911813815
Moneer <moneeralmatary2020@gmail.com>
2023-10-18 20:08:51 +0300

    Add project files.

---
+++ learndocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace learndocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
